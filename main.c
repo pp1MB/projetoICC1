df1fa8cfe5a53dc597411a69d05d42786c2bd922
pp1MB <pedrohsprestes@gmail.com>
2024-05-31 14:06:30 -0300

    Testando input de comandos por struct

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void AV(void);
+void RR(void);
+
+typedef struct {
+    char *key;
+    void (*func)();
+} comando;
 
 int main(void){
+    char inputComando[2]; // Fazer alocação dinâmica ?
 
-    printf("Projeto de ICC1");
-    
+    comando options[] = {
+        {"AV", AV},
+        {"RR", RR},
+        {NULL, NULL} // Posteriormente dá pra remover isso e colocar i<n_comandos possíveis
+    };
+
+    scanf("%s", inputComando); // Pede o comando ao usuário
+
+    // Esse trecho pega uma lista de comandos e compara com o "inputComando", ao achar um comando correspondente ele chama a função desse comando.
+    for(int i=0; options[i].key != NULL; i++){
+        if(strcmp(inputComando, options[i].key) == 0){
+            options[i].func();
+        }
+    }
+
     return 0;
 }
+
+void AV(void){
+    printf("AV");
+}
+
+void RR(void){
+    printf("RR");
+}
+
